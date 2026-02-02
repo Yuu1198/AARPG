37ae9115f8390191353cebddc51a9898b383ba2e
Nadia <nadia.hofer1998@outlook.com>
2026-02-02 17:03:04 +0100

    Player Attack
    Created Attack State. Created Animation with Swoosh. Added Sound.

---
+++ aarpg/Player/Scripts/state_attack.gd
@@ -0,0 +1,65 @@
+class_name State_Attack extends State
+
+var attacking : bool = false
+
+@export var attack_sound : AudioStream
+@export_range(1, 20, 0.5) var decelerate_speed : float = 5.0
+
+@onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
+@onready var attack_anim : AnimationPlayer = $"../../Sprite2D/AttackEffectsSprite/AnimationPlayer"
+@onready var audio : AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
+
+
+@onready var walk : State = $"../Walk"
+@onready var idle = $"../Idle"
+
+
+## What happens when the player enters this State?
+func Enter() -> void:
+	# Animation
+	player.UpdateAnimation("attack")
+	attack_anim.play("attack_" + player.AnimDirection())
+	animation_player.animation_finished.connect(EndAttack) # When animation finished, call EndAttack
+	
+	# Audio
+	audio.stream = attack_sound
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
+	
+	attacking = true
+	pass
+
+
+## What happens when the player exits the State?
+func Exit() -> void:
+	animation_player.animation_finished.disconnect(EndAttack)
+	attacking = false
+	pass
+
+
+## What happens during the _process update in this State?
+func Process(_delta : float) -> State:
+	player.velocity -= player.velocity * decelerate_speed * _delta # Decrease walk speed over time
+	
+	if attacking == false:
+		# When not moving go to Idle State else Walk
+		if player.direction == Vector2.ZERO: 
+			return idle
+		else:
+			return walk
+	return null 
+
+
+## What happens during the _physics_process update in this State?
+func Physics(_delta : float) -> State:
+	return null
+
+
+## What happens with input events in this State?
+func HandleInput(_event : InputEvent) -> State:
+	return null
+
+
+func EndAttack(_nextAnimName : String) -> void:
+	attacking = false
+	pass
