77c8bae97370ae44555c824e11e415fa08451a08
Nadia <nadia.hofer1998@outlook.com>
2026-02-09 14:36:49 +0100

    Enemy Stun and Destroy State
    Enemy can be damaged and it goes into Stun State. Enemy can die and go into Destroy State.

--- aarpg/Enemies/scripts/enemy.gd
+++ aarpg/Enemies/scripts/enemy.gd
@@ -2,6 +2,7 @@ class_name Enemy extends CharacterBody2D
 
 signal direction_changed(new_direction : Vector2)
 signal enemy_damaged()
+signal enemy_destroyed()
 
 const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
 
@@ -14,7 +15,7 @@ var invulnerable : bool = false
 
 @onready var animation_player = $AnimationPlayer
 @onready var sprite = $Sprite2D
-#@onready var hit_box : HitBox = $HitBox
+@onready var hit_box : HitBox = $HitBox
 @onready var state_machine : EnemyStateMachine = $EnemyStateMachine
 
 
@@ -22,6 +23,7 @@ var invulnerable : bool = false
 func _ready():
 	state_machine.initialize(self)
 	player = PlayerManager.player
+	hit_box.damaged.connect(_take_damage)
 	pass # Replace with function body.
 
 
@@ -67,3 +69,13 @@ func anim_direction() -> String:
 		return "up"
 	else:
 		return "side"
+
+
+func _take_damage(damage : int) -> void:
+	if invulnerable == true:
+		return
+	hp -= damage
+	if hp > 0:
+		enemy_damaged.emit()
+	else: # Dead
+		enemy_destroyed.emit() 
